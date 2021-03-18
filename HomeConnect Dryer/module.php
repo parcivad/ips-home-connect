b5ef548f8ee7b41c4648990fb4fb18bfa42707ad
Timur Stegmann <github.timur@4stegmanns.de>
2021-03-18 16:04:18 +0100

    Update Devices

---
+++ HomeConnect Dryer/module.php
@@ -0,0 +1,99 @@
+<?php
+
+  class HomeConnectCoffeeMaker extends IPSModule {
+
+      public function Create() {
+          // Overwrite ips function
+          parent::Create();
+
+          $this->RegisterPropertyString('name', '');
+          $this->RegisterPropertyString('device', '');
+          $this->RegisterPropertyString('company', '');
+          $this->RegisterPropertyString('haId', '');
+      }
+
+      public function ApplyChanges()
+      {
+          // Overwrite ips function
+          parent::ApplyChanges();
+      }
+
+
+      // BUILDING FORM
+      public function GetConfigurationForm()
+      {
+          // return current form
+          $Form = json_encode([
+              'elements' => $this->FormElements(),
+              //'actions'  => $this->FormActions(),
+              'status'   => $this->FormStatus(),
+          ]);
+          $this->SendDebug('FORM', $Form, 0);
+          $this->SendDebug('FORM', json_last_error_msg(), 0);
+
+          return $Form;
+      }
+
+
+
+      /**
+       * @return array[] Form Actions
+       */
+      protected function FormActions() {
+          $form = [
+              [
+
+              ],
+          ];
+
+          return $form;
+      }
+
+      /**
+       * @return array[] Form Elements
+       */
+      protected function FormElements() {
+          $form = [
+              [
+                  "type" => "Image",
+                  "image" => "",
+              ],
+          ];
+
+          return $form;
+      }
+
+      /**
+       * @return array[] Form Status
+       */
+      protected function FormStatus() {
+          $form = [
+              [
+                  'code'    => 101,
+                  'icon'    => 'inactive',
+                  'caption' => 'Creating instance.',
+              ],
+              [
+                  'code'    => 102,
+                  'icon'    => 'active',
+                  'caption' => 'HomeConnect Discovery created.',
+              ],
+              [
+                  'code'    => 104,
+                  'icon'    => 'inactive',
+                  'caption' => 'interface closed.',
+              ],
+              [
+                  'code'    => 201,
+                  'icon'    => 'inactive',
+                  'caption' => 'Please follow the instructions.',
+              ]
+          ];
+
+          return $form;
+      }
+
+
+  }
+
+?>
