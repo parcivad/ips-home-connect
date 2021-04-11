e4d68095e66d97cf308b4e7f754ae7bfb4550ed4
Timur Stegmann <github.timur@4stegmanns.de>
2021-04-11 15:10:03 +0200

    tm

--- HomeConnect Discovery/module.php
+++ HomeConnect Discovery/module.php
@@ -1,229 +1,230 @@
 <?php
 
-  define('__ROOT__', dirname(dirname(__FILE__)));
-  require_once(__ROOT__ . "/libs/tools/HomeConnectApi.php");
-  include(__ROOT__ . "/libs/tools/tm/tm.php");
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__ . "/libs/tools/HomeConnectApi.php");
+require_once(__ROOT__ . "/libs/tools/tm/tm.php");
 
-  class HomeConnectDiscovery extends IPSModule {
+$tm = new tm();
 
-       /*
-       * Internal function of SDK
-       */
-      public function Create()
-      {
-          // Overwrite ips function
-          parent::Create();
+class HomeConnectDiscovery extends IPSModule {
 
-          // User Data
-          $this->RegisterPropertyString("user", "your@mail.de");
-          $this->RegisterPropertyString("password", "password");
-          $this->RegisterPropertyBoolean("simulator", true);
-      }
-      /*
-       * Internal function of SDK
-       */
-      public function ApplyChanges() {
-          // Overwrite ips function
-          parent::ApplyChanges();
-      }
+    /*
+    * Internal function of SDK
+    */
+    public function Create()
+    {
+        // Overwrite ips function
+        parent::Create();
 
-      public function GetDevices() {
+        // User Data
+        $this->RegisterPropertyString("user", "your@mail.de");
+        $this->RegisterPropertyString("password", "password");
+        $this->RegisterPropertyBoolean("simulator", true);
+    }
+    /*
+     * Internal function of SDK
+     */
+    public function ApplyChanges() {
+        // Overwrite ips function
+        parent::ApplyChanges();
+    }
 
-          $api = new HomeConnectApi();
-          $api->SetUser( $this->ReadPropertyString('user') );
-          $api->SetPassword( $this->ReadPropertyString('password') );
-          $api->SetSimulator( $this->ReadPropertyBoolean('simulator') );
+    public function GetDevices() {
 
-          $data = $api->Api("homeappliances");
-          // catch null exception
-          if ( $data == null ) { $error_return = [[ 'name' => 'Login failed [Token/Auth]', 'device' => ' ', 'company' => ' ', 'haId' => 'Überprüfe dein Passwort oder Nutzer/Check your passwort or user', 'connected' => ' ', 'rowColor' => '#ff0000']]; return $error_return;}
-          // else set data source
-          $data = $data['data']['homeappliances'];
+        $api = new HomeConnectApi();
+        $api->SetUser( $this->ReadPropertyString('user') );
+        $api->SetPassword( $this->ReadPropertyString('password') );
+        $api->SetSimulator( $this->ReadPropertyBoolean('simulator') );
 
-          $len = count($data);
+        $data = $api->Api("homeappliances");
+        // catch null exception
+        if ( $data == null ) { $error_return = [[ 'name' => 'Login failed [Token/Auth]', 'device' => ' ', 'company' => ' ', 'haId' => 'Überprüfe dein Passwort oder Nutzer/Check your passwort or user', 'connected' => ' ', 'rowColor' => '#ff0000']]; return $error_return;}
+        // else set data source
+        $data = $data['data']['homeappliances'];
 
-          $devices = [];
+        $len = count($data);
 
-          for ($i = 0; $i < $len; $i++) {
-              array_push($devices, $data[$i] );
-          }
+        $devices = [];
 
-          $config_list = [];
+        for ($i = 0; $i < $len; $i++) {
+            array_push($devices, $data[$i] );
+        }
 
-          if (!empty($devices)) {
-              foreach ($devices as $device) {
-                  $name = $device['name'];
-                  $brand = $device['brand'];
-                  $connected = $device['connected'];
-                  $type = $device['type'];
-                  $haId = $device['haId'];
+        $config_list = [];
 
-                  switch ( $type ) {
-                      case "Dryer":
-                          $module = "{F8AE3556-6835-DD3C-E8E0-F686BE81850D}";
-                          break;
-                      case "Oven":
-                          $module = "{5899C50B-7033-9DA4-BD0A-D8ED2BF227B9}";
-                          break;
-                      case "CoffeeMaker":
-                          $module = "{D5EF280F-8F60-C250-008F-8B17C4B69FD2}";
-                          break;
-                      case "FridgeFreezer":
-                          $module = "{B03C2C23-A59C-026C-AD0B-CEA47312A5AB}";
-                          break;
-                  }
+        if (!empty($devices)) {
+            foreach ($devices as $device) {
+                $name = $device['name'];
+                $brand = $device['brand'];
+                $connected = $device['connected'];
+                $type = $device['type'];
+                $haId = $device['haId'];
+
+                switch ( $type ) {
+                    case "Dryer":
+                        $module = "{F8AE3556-6835-DD3C-E8E0-F686BE81850D}";
+                        break;
+                    case "Oven":
+                        $module = "{5899C50B-7033-9DA4-BD0A-D8ED2BF227B9}";
+                        break;
+                    case "CoffeeMaker":
+                        $module = "{D5EF280F-8F60-C250-008F-8B17C4B69FD2}";
+                        break;
+                    case "FridgeFreezer":
+                        $module = "{B03C2C23-A59C-026C-AD0B-CEA47312A5AB}";
+                        break;
+                }
 
 
-                  $config_list[] = [
-                      'name' => $name,
-                      'device' => $type,
-                      'company' => $brand,
-                      'haId' => $haId,
-                      'connected' => $connected,
-                      'create'     => [
-                          'moduleID'      => $module,
-                          'configuration' => [
-                              'name' => $name,
-                              'device' => $type,
-                              'company' => $brand,
-                              'haId' => $haId,
-                          ],
-                      ],
-                  ];
-              }
-          }
+                $config_list[] = [
+                    'name' => $name,
+                    'device' => $type,
+                    'company' => $brand,
+                    'haId' => $haId,
+                    'connected' => $connected,
+                    'create'     => [
+                        'moduleID'      => $module,
+                        'configuration' => [
+                            'name' => $name,
+                            'device' => $type,
+                            'company' => $brand,
+                            'haId' => $haId,
+                        ],
+                    ],
+                ];
+            }
+        }
 
-          return  $config_list;
-      }
+        return  $config_list;
+    }
 
-      public function GetConfigurationForm()
-      {
-          // return current form
-          $Form = json_encode([
-              'elements' => $this->FormElements(),
-              //'actions'  => $this->FormActions(),
-              'status'   => $this->FormStatus(),
-          ]);
-          $this->SendDebug('FORM', $Form, 0);
-          $this->SendDebug('FORM', json_last_error_msg(), 0);
+    public function GetConfigurationForm()
+    {
+        // return current form
+        $Form = json_encode([
+            'elements' => $this->FormElements(),
+            //'actions'  => $this->FormActions(),
+            'status'   => $this->FormStatus(),
+        ]);
+        $this->SendDebug('FORM', $Form, 0);
+        $this->SendDebug('FORM', json_last_error_msg(), 0);
 
-          return $Form;
-      }
+        return $Form;
+    }
 
-      /**
-       * @return array[] Form Actions
-       */
-      protected function FormActions() {
-          $form = [
-              [
+    /**
+     * @return array[] Form Actions
+     */
+    protected function FormActions() {
+        $form = [
+            [
 
-              ],
-          ];
+            ],
+        ];
 
-          return $form;
-      }
+        return $form;
+    }
 
-      /**
-       * @return array[] Form Elements
-       */
-      protected function FormElements() {
-          $form = [
-              [
-                  "type" => "Button",
-                  "caption" => "Authorize your user",
-                  "onClick" => 'authorize("https://simulator.home-connect.com/security/oauth/authorize", "35C7EC3372C6EB5FB5378505AB9CE083D80A97713698ACB07B20C6E41E5E2CD5", "IdentifyAppliance");'
-              ],
-              [
-                  "type" => "ValidationTextBox",
-                  "name" => "user",
-                  "caption" => "HomeConnect - User-Email",
-              ],
-              [
-                  "type" => "PasswordTextBox",
-                  "name" => "password",
-                  "caption" => "HomeConnect - Password",
-              ],
-              [
-                  "type" => "CheckBox",
-                  "name" => "simulator",
-                  "caption" => "HomeConnect Simulation verwenden."
-              ],
-              [
-                  "type" => "Configurator",
-                  "name" => "Home-Connect Discovery",
-                  "caption" => "HomeConnect Discovery",
-                  "rowCount" => 14,
-                  "add" => false,
-                  "delete"=> true,
-                  "columns" => [
-                      [
-                          "caption" => "Name",
-                          "name" => "name",
-                          "width" => "200px",
-                          "add" => false,
-                      ],
-                      [
-                          "caption" => "Device",
-                          "name" => "device",
-                          "width" => "120px",
-                          "add" => false,
-                      ],
-                      [
-                          "caption" => "Company",
-                          "name" => "company",
-                          "width" => "120px",
-                          "add" => false,
-                      ],
-                      [
-                          "caption" => "haId",
-                          "name" => "haId",
-                          "width" => "auto",
-                          "add" => false,
-                      ],
-                      [
-                          "caption" => "Connection",
-                          "name" => "connected",
-                          "width" => "100px",
-                          "add" => false,
-                      ],
-                  ],
-                  "values" => $this->GetDevices(),
-              ],
-          ];
+    /**
+     * @return array[] Form Elements
+     */
+    protected function FormElements() {
+        $form = [
+            [
+                "type" => "Button",
+                "caption" => "Authorize your user",
+                "onClick" => '$tm->authorize("https://simulator.home-connect.com/security/oauth/authorize", "35C7EC3372C6EB5FB5378505AB9CE083D80A97713698ACB07B20C6E41E5E2CD5", "IdentifyAppliance");'
+            ],
+            [
+                "type" => "ValidationTextBox",
+                "name" => "user",
+                "caption" => "HomeConnect - User-Email",
+            ],
+            [
+                "type" => "PasswordTextBox",
+                "name" => "password",
+                "caption" => "HomeConnect - Password",
+            ],
+            [
+                "type" => "CheckBox",
+                "name" => "simulator",
+                "caption" => "HomeConnect Simulation verwenden."
+            ],
+            [
+                "type" => "Configurator",
+                "name" => "Home-Connect Discovery",
+                "caption" => "HomeConnect Discovery",
+                "rowCount" => 14,
+                "add" => false,
+                "delete"=> true,
+                "columns" => [
+                    [
+                        "caption" => "Name",
+                        "name" => "name",
+                        "width" => "200px",
+                        "add" => false,
+                    ],
+                    [
+                        "caption" => "Device",
+                        "name" => "device",
+                        "width" => "120px",
+                        "add" => false,
+                    ],
+                    [
+                        "caption" => "Company",
+                        "name" => "company",
+                        "width" => "120px",
+                        "add" => false,
+                    ],
+                    [
+                        "caption" => "haId",
+                        "name" => "haId",
+                        "width" => "auto",
+                        "add" => false,
+                    ],
+                    [
+                        "caption" => "Connection",
+                        "name" => "connected",
+                        "width" => "100px",
+                        "add" => false,
+                    ],
+                ],
+                "values" => $this->GetDevices(),
+            ],
+        ];
 
-          return $form;
-      }
+        return $form;
+    }
 
-      /**
-       * @return array[] Form Status
-       */
-      protected function FormStatus() {
-          $form = [
-              [
-                  'code'    => 101,
-                  'icon'    => 'inactive',
-                  'caption' => 'Creating instance.',
-              ],
-              [
-                  'code'    => 102,
-                  'icon'    => 'active',
-                  'caption' => 'HomeConnect Discovery created.',
-              ],
-              [
-                  'code'    => 104,
-                  'icon'    => 'inactive',
-                  'caption' => 'interface closed.',
-              ],
-              [
-                  'code'    => 201,
-                  'icon'    => 'inactive',
-                  'caption' => 'Please follow the instructions.',
-              ],
-          ];
+    /**
+     * @return array[] Form Status
+     */
+    protected function FormStatus() {
+        $form = [
+            [
+                'code'    => 101,
+                'icon'    => 'inactive',
+                'caption' => 'Creating instance.',
+            ],
+            [
+                'code'    => 102,
+                'icon'    => 'active',
+                'caption' => 'HomeConnect Discovery created.',
+            ],
+            [
+                'code'    => 104,
+                'icon'    => 'inactive',
+                'caption' => 'interface closed.',
+            ],
+            [
+                'code'    => 201,
+                'icon'    => 'inactive',
+                'caption' => 'Please follow the instructions.',
+            ],
+        ];
 
-          return $form;
-      }
+        return $form;
+    }
 
-  }
-
+}
 ?>
---
+++ libs/tools/tm/incoming.php
@@ -0,0 +1,28 @@
+<?php
+
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__ . "/tm/tm.php");
+
+$tm = new tm();
+
+$redirect_params = explode("?", $_SERVER["REQUEST_URI"])[1];
+
+if ( isset( $redirect_params ) ) {
+
+    // check if the code is in the return to the localhost
+    if ( explode( "=", $redirect_params )[0] == "code") {
+        $code = str_replace( "=", "", urldecode( explode("=", explode("&", $redirect_params)[0])[1] ));
+        // user feedback
+        echo("<p>authorized!</p>");
+        // save code
+        $tm->write( "code", $code, true);
+        // stop php server
+        shell_exec("killall -9 php");
+        // return code for developer
+        return $code;
+    } else {
+        throw new UnexpectedValueException("Error from your api: " . $redirect_params);
+    }
+} else {
+    throw new UnexpectedValueException("No Parameters after authorization check your url and client id!");
+}
