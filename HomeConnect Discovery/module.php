52ab7be954faf2b67e552edde753de57a3ed80ca
Timur Stegmann <github.timur@4stegmanns.de>
2021-04-11 20:17:24 +0200

    Update module.php

--- HomeConnect Discovery/module.php
+++ HomeConnect Discovery/module.php
@@ -48,8 +48,9 @@ class HomeConnectDiscovery extends IPSModule {
 
     public function GetDevices() {
 
-
-        getToken("https://simulator.home-connect.com/security/oauth/token", "35C7EC3372C6EB5FB5378505AB9CE083D80A97713698ACB07B20C6E41E5E2CD5", "EC9B4140CB439DF1BEEE39860141077C92C553AC65FEE729B88B7092B745B1F7");
+        if ( getAuthorizeCode() ) {
+            getToken("https://simulator.home-connect.com/security/oauth/token", "35C7EC3372C6EB5FB5378505AB9CE083D80A97713698ACB07B20C6E41E5E2CD5", "EC9B4140CB439DF1BEEE39860141077C92C553AC65FEE729B88B7092B745B1F7");
+        }
 
         $data = Api("homeappliances");
         // catch null exception
