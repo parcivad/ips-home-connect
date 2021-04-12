deb21ea41a188a8fc478fe719fec894b0b90371b
Timur Stegmann <github.timur@4stegmanns.de>
2021-04-12 11:48:25 +0200

    Update module.php

--- HomeConnect Discovery/module.php
+++ HomeConnect Discovery/module.php
@@ -2,6 +2,7 @@
 
 require_once( dirname(dirname(__FILE__) ) . "/libs/tools/HomeConnectApi.php");
 require_once( dirname(dirname(__FILE__) ) . "/libs/tools/tm/tm.php");
+define( 'data', json_decode( file_get_contents("data.json", dirname(dirname(__FILE__) ) . "/libs/tools/tm/data.json" ), true ) );
 
 
 class HomeConnectDiscovery extends IPSModule {
@@ -55,9 +56,9 @@ class HomeConnectDiscovery extends IPSModule {
 
     public function GetDevices() {
 
-        $this->SendDebug("starting...", getAuthorizeCode(), 0);
-        if ( is_string( getAuthorizeCode() ) ) {
-            $this->SendDebug("startet authorization", getAuthorizeCode(), 0);
+        $this->SendDebug("starting...", data["authorize"]["code"], 0);
+        if ( is_string( data["authorize"]["code"] ) ) {
+            $this->SendDebug("startet authorization", data["authorize"]["code"], 0);
             getToken("https://api.home-connect.com/security/oauth/token", "35C7EC3372C6EB5FB5378505AB9CE083D80A97713698ACB07B20C6E41E5E2CD5", "EC9B4140CB439DF1BEEE39860141077C92C553AC65FEE729B88B7092B745B1F7");
         } else {
             $this->SendDebug("no clue", "", 0);
