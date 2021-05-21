3f97b965497e6d4472d153e377cb3b7dba86f89e
Timur Stegmann <github.timur@4stegmanns.de>
2021-05-21 22:41:49 +0200

    Update module.php

--- HomeConnect Discovery/module.php
+++ HomeConnect Discovery/module.php
@@ -187,13 +187,13 @@ class HomeConnectDiscovery extends IPSModule {
             [
                 "type" => "Button",
                 "caption" => "Logout",
-                "onClick" => 'HomeConnectDiscovery_tm( $id, "reset", " );',
+                "onClick" => 'HomeConnectDiscovery_tm( $id, "reset" );',
                 'confirm' => 'Bist du sicher, dass du dich ausloggen willst.'
             ],
             [
                 "type" => "Button",
                 "caption" => "Login",
-                "onClick" => 'HomeConnectDiscovery_tm( $id, "auth, "" );',
+                "onClick" => 'HomeConnectDiscovery_tm( $id, "auth" );',
             ]
         ];
     }
