9763e30cb1c9c200a84a7c3f9d5cadc56d59bcfc
Timur Stegmann <github.timur@4stegmanns.de>
2021-09-03 17:12:45 +0200

    s

--- HomeConnect Discovery/module.php
+++ HomeConnect Discovery/module.php
@@ -230,7 +230,7 @@ class HomeConnectDiscovery extends IPSModule {
                     [
                         "type" => "Label",
                         "name" => "loginInfo",
-                        "caption" => "Open the link in a browser; Log in and copy the url out of your browser after it shows no page (address should look like this: http://localhost:8080...)",
+                        "caption" => "Open the link in a browser; Log in and copy the url out of your browser after it shows no page.",
                         "visible" => $visible,
                     ],
                     [
