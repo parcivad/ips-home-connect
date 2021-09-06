1d0bf232cde7eedbb98e46622c10fa2fe5bb42a3
Timur Stegmann <github.timur@4stegmanns.de>
2021-09-06 12:05:15 +0200

    Update module.php

--- HomeConnect Splitter/module.php
+++ HomeConnect Splitter/module.php
@@ -78,7 +78,7 @@ class HomeConnectSplitter extends IPSModule {
         try {
             $token = getToken("https://api.home-connect.com/security/oauth/token", "35C7EC3372C6EB5FB5378505AB9CE083D80A97713698ACB07B20C6E41E5E2CD5", "EC9B4140CB439DF1BEEE39860141077C92C553AC65FEE729B88B7092B745B1F7");
         } catch (Exception $ex) {
-            throw new Exception($ex->getMessage());
+            $token = "PUSH-BUTTON-IN-MENU";
         }
         // get parent instance
         $parent = IPS_GetInstance( $this->InstanceID )['ConnectionID'];
